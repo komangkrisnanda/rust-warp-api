ec9a2673c86a6a1d3109fd39014ae1d66503ae28
𝚔𝚘𝚖𝚊𝚗𝚐 𝚔𝚛𝚒𝚜𝚗𝚊𝚗𝚍𝚊 <krisnanda@fgi.co.id>
2024-05-29 08:18:57 +0800

    chore: setup env

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,25 @@ mod handlers;
 mod models;
 mod routes;
 
+use dotenv::dotenv;
+use std::env;
+
 #[tokio::main]
 async fn main() {
+    //Load env.
+
+    dotenv().ok();
+
+    //Validate env.
+    let app_port: u16 = env::var("APP_PORT")
+        .expect("APP_PORT must be set.")
+        .parse()
+        .expect("APP_PORT must be a valid u16 number.");
+
+    //Map routes.
     let routes = routes::routes();
 
-    println!("Server started at https://localhost:8000");
-    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    //Serve app.
+    println!("Server started at http://localhost:{}", app_port);
+    warp::serve(routes).run(([127, 0, 0, 1], app_port)).await;
 }
